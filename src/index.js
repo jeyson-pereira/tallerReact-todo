caea85913ad8cb4816097cb78db4263c38fa5bb9
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-05-02 12:19:13 -0500

    Cambio en iconos con FontAwesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/index.js';
 import './index.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(
   <App />,
