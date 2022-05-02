1869142c9a555fb2c3b045c965f3cff0447e4a18
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-05-02 12:29:49 -0500

    cambios TodoSearch Componente
    Se hizo modificación de estilos y texto en placeholder + nuevo icono

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="&#xF002; Buscar TODOs..."
       value={searchValue}
       onChange={onSearchValueChange}
     />
